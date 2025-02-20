06d7adde5d1878ce1330ec39ec5c442d947e8bf5
Mohamed Houssas <mhoussas26@gmail.com>
2025-02-20 21:46:02 +0100

    Complete Sorting In Export CMD

--- ft_export.c
+++ ft_export.c
@@ -6,7 +6,7 @@
 /*   By: mhoussas <mhoussas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:42:43 by mhoussas          #+#    #+#             */
-/*   Updated: 2025/02/19 21:40:57 by mhoussas         ###   ########.fr       */
+/*   Updated: 2025/02/20 21:44:35 by mhoussas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,31 @@
 
 static char	**ft_sort(char **lst)
 {
+	int	len;
+	int	i;
+	int	j;
+	char *tmp;
 
+	len = 0;
+	while (lst[len])
+		len++;
+	i = 0;
+	while (lst[i+1])
+	{
+		j = i + 1;
+		while (lst[j])
+		{
+			if (ft_strncmp(lst[i], lst[j], INT_MAX) > 0)
+			{
+				tmp = lst[i];
+				lst[i] = lst[j];
+				lst[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (lst);
 }
 
 void	ft_export(char **args, char **env)
